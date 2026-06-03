662550636be935ce3532ffc6d483a2c9b790b530
John Doe <johndoe@example.com>
2026-06-03 22:42:19 +0200

    step 2 layout business

--- app/Livewire/Business/Booking/Step2.php
+++ app/Livewire/Business/Booking/Step2.php
@@ -151,6 +151,7 @@ class Step2 extends Component
 
     public function render()
     {
-        return view('livewire.business.booking.step2');
+        return view('livewire.business.booking.step2')
+         ->layout('layouts.business', ['business' => $this->business]);
     }
 }
